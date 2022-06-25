714e2604d5a80ab5985bab7ad2906d2d7f2b843f
DanielRistoff <daniel_ristoff@hotmail.com>
2022-06-25 16:29:46 -0300

    add CupertinoPicker

--- carronamao/lib/car_in_hand_app/ui_view/add_sticky_notes_view.dart
+++ carronamao/lib/car_in_hand_app/ui_view/add_sticky_notes_view.dart
@@ -1,6 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../car_in_hand_app_theme.dart';
+
+List<String> getDates() {
+  final DateFormat formatter = DateFormat('dd-MM-yyyy');
+  final DateTime now = DateTime.now();
+  List<String> datesF = <String>[formatter.format(now)];
+
+  for (var i = 1; i < 31; i++) {
+    datesF.add(formatter.format(DateTime(now.year, now.month, now.day + i)));
+  }
+  return datesF;
+}
+
+List<String> getHours() {
+  final DateFormat formatter = DateFormat('HH:mm');
+  final DateTime now = DateTime.now();
+  final DateTime initialDate = DateTime(now.year, now.month, now.day, 0, 0);
+  List<String> hoursF = <String>[formatter.format(initialDate)];
+
+  for (var i = 1; i < 48; i++) {
+    hoursF.add(formatter.format(DateTime(initialDate.year, initialDate.month,
+        initialDate.day, initialDate.hour, initialDate.minute + 30 * i)));
+  }
+  return hoursF;
+}
+
+List<String> _dates = getDates();
+List<String> _hours = getHours();
+
+const List<String> _services = <String>[
+  'Troca de óleo',
+  'Troca de filtro de óleo',
+  'Troca de filtro de ar',
+  'Troca de filtro de gasolina',
+  'Calibrar pneu',
+  'Revisão básica',
+  'Revisão Completa',
+  'Revisão freios',
+  'Outros',
+];
+
+int selectDate = 0;
+int selectHours = 0;
+int selectService = 0;
 
 class AddStickyNotesView extends StatelessWidget {
   const AddStickyNotesView({Key? key}) : super(key: key);
@@ -61,16 +105,30 @@ class AddStickyNotesView extends StatelessWidget {
                             ],
                           ),
                           Row(
-                            children: const <Widget>[
+                            children: <Widget>[
                               Padding(
-                                  padding: EdgeInsets.only(
+                                  padding: const EdgeInsets.only(
                                     left: 30,
                                     right: 16,
                                     top: 2,
                                   ),
                                   child: SizedBox(
                                     width: 300,
-                                    child: CupertinoTextField(),
+                                    height: 70,
+                                    child: CupertinoPicker(
+                                      itemExtent: 30,
+                                      onSelectedItemChanged: (int indexDate) {
+                                        selectDate = indexDate;
+                                      },
+                                      children: List<Widget>.generate(
+                                          _dates.length, (int index) {
+                                        return Center(
+                                          child: Text(
+                                            _dates[index],
+                                          ),
+                                        );
+                                      }),
+                                    ),
                                   )),
                             ],
                           ),
@@ -97,16 +155,30 @@ class AddStickyNotesView extends StatelessWidget {
                             ],
                           ),
                           Row(
-                            children: const <Widget>[
+                            children: <Widget>[
                               Padding(
-                                  padding: EdgeInsets.only(
+                                  padding: const EdgeInsets.only(
                                     left: 30,
                                     right: 16,
-                                    top: 2,
+                                    top: 10,
                                   ),
                                   child: SizedBox(
                                     width: 300,
-                                    child: CupertinoTextField(),
+                                    height: 70,
+                                    child: CupertinoPicker(
+                                      itemExtent: 30,
+                                      onSelectedItemChanged: (int indexHour) {
+                                        selectHours = indexHour;
+                                      },
+                                      children: List<Widget>.generate(
+                                          _hours.length, (int index) {
+                                        return Center(
+                                          child: Text(
+                                            _hours[index],
+                                          ),
+                                        );
+                                      }),
+                                    ),
                                   )),
                             ],
                           ),
@@ -133,16 +205,31 @@ class AddStickyNotesView extends StatelessWidget {
                             ],
                           ),
                           Row(
-                            children: const <Widget>[
+                            children: <Widget>[
                               Padding(
-                                  padding: EdgeInsets.only(
+                                  padding: const EdgeInsets.only(
                                     left: 30,
                                     right: 16,
-                                    top: 2,
+                                    top: 10,
                                   ),
                                   child: SizedBox(
                                     width: 300,
-                                    child: CupertinoTextField(),
+                                    height: 70,
+                                    child: CupertinoPicker(
+                                      itemExtent: 30,
+                                      onSelectedItemChanged:
+                                          (int indexService) {
+                                        selectService = indexService;
+                                      },
+                                      children: List<Widget>.generate(
+                                          _services.length, (int index) {
+                                        return Center(
+                                          child: Text(
+                                            _services[index],
+                                          ),
+                                        );
+                                      }),
+                                    ),
                                   )),
                             ],
                           ),
